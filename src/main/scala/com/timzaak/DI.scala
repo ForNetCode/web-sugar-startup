f570a27212bbdfbf334ae1d0441c670b7c44244c
timzaak <zsy.evan@gmail.com>
2024-07-15 12:11:48 +0800

    remove quill

--- src/main/scala/com/timzaak/DI.scala
+++ src/main/scala/com/timzaak/DI.scala
@@ -1,11 +1,7 @@
 package com.timzaak
 
-import com.timzaak.dao.DB
 import very.util.config.WithConfig
 
 object DI extends WithConfig {
-
-  // init postgres
-  // object db extends DB
-  // give DB = db
+  
 }
--- src/main/scala/com/timzaak/dao/DB.scala
+++ src/main/scala/com/timzaak/dao/DB.scala
@@ -1,8 +1,1 @@
 package com.timzaak.dao
-
-import io.getquill.*
-import very.util.persistence.quill.{IDSupport, PageSupport, CirceJsonSupport}
-
-
-
-class DB extends PostgresJdbcContext(SnakeCase, "database") with CirceJsonSupport with IDSupport with PageSupport[SnakeCase]
