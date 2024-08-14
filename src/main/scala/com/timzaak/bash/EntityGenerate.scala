b8b4f1b36eb22d3f92eda44ba6db661dddfd04c5
timzaak <zsy.evan@gmail.com>
2024-08-14 17:14:43 +0800

    add entity generate

---
+++ src/main/scala/com/timzaak/bash/EntityGenerate.scala
@@ -0,0 +1,33 @@
+package com.timzaak.bash
+
+import com.typesafe.config.ConfigFactory
+import very.util.persistence.scalikejdbc.mapper.{CodeGenerator, DateTimeClass, GeneratorConfig, Model}
+
+object EntityGenerate {
+  def main(args: Array[String]): Unit = {
+    val config = ConfigFactory.load()
+    val url = config.getString("db.default.url")
+    val username = config.getString("db.default.user")
+    val password = config.getString("db.default.password")
+    val driver = config.getString("db.default.driver")
+    Class.forName(driver)
+
+    val model = Model(url, username, password)
+
+    val generatorConfig = GeneratorConfig(
+      packageName = "com.timzaak.entity",
+      dateTimeClass = DateTimeClass.JodaDateTime,
+      daoExtendImport = Some("very.util.persistence.scalikejdbc.Dao")
+    )
+    // where table to generate
+    val tables:Map[String, String] = Map(
+      //      "consignor"-> "Consignor",
+      //      "fee_config" -> "FeeConfig",
+    )
+    model.allTables("public").foreach { table =>
+      if (tables.contains(table.name)) {
+        new CodeGenerator(table, None)(generatorConfig.copy(tableNameToClassName = (a: String) => tables(a))).writeModel()
+      }
+    }
+  }
+}
